92cf7714816d95fcf0a2edf0e3b5ddd24ef53058
Agrima Shukla <Agrima.Shukla@watchguard.com>
2024-07-05 14:08:32 +0530

    Minor changes

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,12 @@ import java.util.logging.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     public static void main(String[] args) {
-
-        FileHandler fh;
-
-        try {
-
-            fh = new FileHandler("C:/UserS/ashukla/Desktop/Java/mini-project/Clinic_manager_Java/MyLogFile.log");
-            SimpleFormatter formatter = new SimpleFormatter();
-            fh.setFormatter(formatter);
-            LOGGER.addHandler(fh);
-            LOGGER.log(Level.INFO, "Start of application");
 
             DatabaseConnection conn = DatabaseConnection.getInstance();        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
             MainMenu.showMainMenu();
             DatabaseConnection.destroyInstance();
-
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
 
     }
 }
